a2f130c30dc46f27b6fe26ec9b37e7f6371252f9
StephenWang123 <stephenwang380@gmail.com>
2018-02-13 23:39:17 -0800

    Journal function works roughly

--- app/src/main/java/com/example/g/luciddreamgenerator/JournalActivity.java
+++ app/src/main/java/com/example/g/luciddreamgenerator/JournalActivity.java
@@ -8,8 +8,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class JournalActivity extends AppCompatActivity {
+
+    EditText myEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +51,18 @@ public class JournalActivity extends AppCompatActivity {
         save_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(JournalActivity.this, MainActivity.class));
+                myEdit = (EditText)findViewById(R.id.editText2);
+                String content = myEdit.getText().toString();
+                if (content.isEmpty())
+                {
+                    Toast.makeText(getBaseContext(), "Can't Save Empty Dream Log!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Intent intent = new Intent(getBaseContext(), JournalListActivity.class);
+
+                intent.putExtra("DREAM_CONTENT", content);
+                startActivity(intent);
+                //startActivity(new Intent(JournalActivity.this, MainActivity.class));
             }
         });
     }
--- app/src/main/java/com/example/g/luciddreamgenerator/JournalListActivity.java
+++ app/src/main/java/com/example/g/luciddreamgenerator/JournalListActivity.java
@@ -1,6 +1,7 @@
 package com.example.g.luciddreamgenerator;
 
 
+import android.app.Activity;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,12 +13,14 @@ import android.widget.ListView;
 import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
+import java.util.List;
+import java.util.LinkedList;
 
 
-public class JournalListActivity extends ListActivity {
+public class JournalListActivity extends Activity {
 
     private ListView dream_list;
-
+    List<String> dreams = new LinkedList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,19 +37,28 @@ public class JournalListActivity extends ListActivity {
             }
         });
 
-        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
-                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
-                "Linux", "OS/2" };
+        if (getIntent() != null) {
+            String new_dream = getIntent().getStringExtra("DREAM_CONTENT");
+            dreams.add(new_dream);
+        }
+
+        String[] dreams_array = new String[dreams.size()];
+        dreams.toArray(dreams_array);
+
+        //dreams_array
+
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, values);
-        setListAdapter(adapter);
+                android.R.layout.simple_list_item_1, dreams_array);
+
+        dream_list = (ListView) findViewById(R.id.list);
+        dream_list.setAdapter(adapter);
 
     }
-
+/*
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
         String item = (String) getListAdapter().getItem(position);
         Toast.makeText(this, item + " selected", Toast.LENGTH_LONG).show();
     }
-
+*/
 }
