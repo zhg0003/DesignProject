c8df2c63725bb02a6173a5b8b14eb5a3953393da
Sean Patrick Malloy <spmalloy@ucdavis.edu>
2018-02-21 16:29:51 -0800

    Simple Javascript
    Simple geometry

---
+++ script.js
@@ -0,0 +1,11 @@
+function submitfunc(){
+    document.getElementById("finish").innerHTML = "Submitted!";
+}
+
+function signup(){
+    document.getElementById("signed").innerHTML = "Signed up!";
+}
+
+function logon(){
+    document.getElementById("logon").innerHTML = "Logged on!"
+}
