1ffd76e9c2507df48540d301babf6aa606713611
StephenWang123 <stephenwang380@gmail.com>
2018-04-28 19:23:22 -0700

    Added delete feature + fixed bugs

--- app/src/main/java/com/example/g/luciddreamgenerator/JournalActivity.java
+++ app/src/main/java/com/example/g/luciddreamgenerator/JournalActivity.java
@@ -17,9 +17,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.Calendar;
+import java.util.LinkedList;
+import java.util.List;
 
 
 public class JournalActivity extends AppCompatActivity {
+
+    List<String> dreams = new LinkedList<String>();
 
     CheckBox checkbox1;
     CheckBox checkbox2;
@@ -91,14 +95,32 @@ public class JournalActivity extends AppCompatActivity {
         String loaded_dreams = settings.getString("fields", "");
         temp_loaded_fields = loaded_dreams.split("~");
         if (temp_loaded_fields.length == 6) {
-            myEdit1.setText(temp_loaded_fields[0]);
-            myEdit2.setText(temp_loaded_fields[1]);
-            myEdit3.setText(temp_loaded_fields[2]);
-            myEdit4.setText(temp_loaded_fields[3]);
-            myEdit5.setText(temp_loaded_fields[4]);
-            myEdit6.setText(temp_loaded_fields[5]);
-
+            if (temp_loaded_fields[0].equals("`"))
+                myEdit1.setText("");
+            else
+                myEdit1.setText(temp_loaded_fields[0]);
+            if (temp_loaded_fields[1].equals("`"))
+                myEdit2.setText("");
+            else
+                myEdit2.setText(temp_loaded_fields[1]);
+            if (temp_loaded_fields[2].equals("`"))
+                myEdit3.setText("");
+            else
+                myEdit3.setText(temp_loaded_fields[2]);
+            if (temp_loaded_fields[3].equals("`"))
+                myEdit4.setText("");
+            else
+                myEdit4.setText(temp_loaded_fields[3]);
+            if (temp_loaded_fields[4].equals("`"))
+                myEdit5.setText("");
+            else
+                myEdit5.setText(temp_loaded_fields[4]);
+            if (temp_loaded_fields[5].equals("`"))
+                myEdit6.setText("");
+            else
+                myEdit6.setText(temp_loaded_fields[5]);
         }
+
     }
 
 
@@ -106,12 +128,30 @@ public class JournalActivity extends AppCompatActivity {
         SharedPreferences settings = getApplicationContext().getSharedPreferences("stored_fields", 0);
         SharedPreferences.Editor editor = settings.edit();
         StringBuilder sb = new StringBuilder();
-        sb.append(myEdit1.getText().toString()).append("~");
-        sb.append(myEdit2.getText().toString()).append("~");
-        sb.append(myEdit3.getText().toString()).append("~");
-        sb.append(myEdit4.getText().toString()).append("~");
-        sb.append(myEdit5.getText().toString()).append("~");
-        sb.append(myEdit6.getText().toString()).append("~");
+        if (myEdit1.getText().toString().isEmpty())
+            sb.append("`").append("~");
+        else
+            sb.append(myEdit1.getText().toString()).append("~");
+        if (myEdit2.getText().toString().isEmpty())
+            sb.append("`").append("~");
+        else
+            sb.append(myEdit2.getText().toString()).append("~");
+        if (myEdit3.getText().toString().isEmpty())
+            sb.append("`").append("~");
+        else
+            sb.append(myEdit3.getText().toString()).append("~");
+        if (myEdit4.getText().toString().isEmpty())
+            sb.append("`").append("~");
+        else
+            sb.append(myEdit4.getText().toString()).append("~");
+        if (myEdit5.getText().toString().isEmpty())
+            sb.append("`").append("~");
+        else
+            sb.append(myEdit5.getText().toString()).append("~");
+        if (myEdit6.getText().toString().isEmpty())
+            sb.append("`").append("~");
+        else
+            sb.append(myEdit6.getText().toString()).append("~");
 
 
         editor.putString("fields", sb.toString());
@@ -161,12 +201,23 @@ public class JournalActivity extends AppCompatActivity {
                     Toast.makeText(getBaseContext(), "Can't Save Empty Dream Log!", Toast.LENGTH_SHORT).show();
                     return;
                 }
+
+                if ( (content1.isEmpty() && checkbox1.isChecked()) ||
+                        (content2.isEmpty() && checkbox2.isChecked()) ||
+                        (content3.isEmpty() && checkbox3.isChecked()) ||
+                        (content4.isEmpty() && checkbox4.isChecked()) ||
+                        (content5.isEmpty() && checkbox5.isChecked()) ||
+                        (content6.isEmpty() && checkbox6.isChecked()) ) {
+                    Toast.makeText(getBaseContext(), "Can't Check Empty Custom Field!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
                 Intent intent = new Intent(getBaseContext(), JournalListActivity.class);
 
                 Date c = Calendar.getInstance().getTime();
                 SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
                 String formattedDream = df.format(c);
-                Toast.makeText(getBaseContext(), formattedDream, Toast.LENGTH_SHORT).show();
+               // Toast.makeText(getBaseContext(), formattedDream, Toast.LENGTH_SHORT).show();
 
                 formattedDream = formattedDream.concat(" - \"");
                 formattedDream = formattedDream.concat(content);
@@ -186,15 +237,43 @@ public class JournalActivity extends AppCompatActivity {
                     formattedDream = formattedDream.concat("\n" + content6);
 
 
-
                 if (dreamRating.getRating() != 0)
                     formattedDream = formattedDream.concat("\nDream Quality: " + String.valueOf(dreamRating.getRating()) + " stars.");
 
                 saveCustomFields();
-                intent.putExtra("DREAM_CONTENT", formattedDream);
-                startActivity(intent);
+                loadDreams();
+                dreams.add(formattedDream);
+                saveDreams();
+                //intent.putExtra("DREAM_CONTENT", formattedDream);
+                //startActivity(intent);
+
+                Toast.makeText(getBaseContext(), "Journal Entry Added.", Toast.LENGTH_LONG).show();
+                startActivity(new Intent(JournalActivity.this, MenuActivity.class));
             }
         });
+    }
+
+    protected void loadDreams() {
+        String[] temp_loaded_dreams;
+        SharedPreferences settings = getApplicationContext().getSharedPreferences("stored_dreams", 0);
+        String loaded_dreams = settings.getString("dreams", "");
+        temp_loaded_dreams = loaded_dreams.split("~>_");
+        dreams = new ArrayList<String>(Arrays.asList(temp_loaded_dreams));
+
+        if (temp_loaded_dreams[0].isEmpty()) // remove initial empty dream
+            dreams.clear();
+    }
+
+
+    protected void saveDreams() {
+        SharedPreferences settings = getApplicationContext().getSharedPreferences("stored_dreams", 0);
+        SharedPreferences.Editor editor = settings.edit();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < dreams.size(); i++) {
+            sb.append(dreams.get(i)).append("~>_");
+        }
+        editor.putString("dreams", sb.toString());
+        editor.apply();
     }
 
 }
--- app/src/main/java/com/example/g/luciddreamgenerator/JournalListActivity.java
+++ app/src/main/java/com/example/g/luciddreamgenerator/JournalListActivity.java
@@ -4,8 +4,10 @@ package com.example.g.luciddreamgenerator;
 import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.KeyEvent;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.ArrayAdapter;
@@ -16,6 +18,7 @@ import java.util.Arrays;
 import java.util.ArrayList;
 
 
+
 import java.util.List;
 import java.util.LinkedList;
 
@@ -23,7 +26,6 @@ import java.util.LinkedList;
 public class JournalListActivity extends ListActivity {
 
     private ListView dream_list;
-    //String[] days = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
     List<String> dreams = new LinkedList<String>();
     ArrayAdapter<String> adapter;
 
@@ -31,38 +33,37 @@ public class JournalListActivity extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_journal_list);
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setUpBackButton();
-        loadDreams();
+        setUpDeleteButton();
 
-        if (getIntent().getStringExtra("DREAM_CONTENT") != null) {
+        Button deleteButton = (Button) findViewById(R.id.button11);
+        deleteButton.setAlpha(0.5f);
+        deleteButton.setClickable(false);
+
+        loadDreams(); // Here we load dreams from local storage into ram
+
+        if (getIntent().getStringExtra("DREAM_CONTENT") != null) { // If we just wrote down a new dream, get that new dream
             String new_dream = getIntent().getStringExtra("DREAM_CONTENT");
             dreams.add(new_dream);
         }
 
-        /*if (dreams.size() < 1) {
-            String empty = "Dream Journal";
-            dreams.add(empty);
-        }*/
-
-        String[] dreams_array = new String[dreams.size()];
+        String[] dreams_array = new String[dreams.size()]; // The rest of onCreate sets up dream_list
         dreams_array = dreams.toArray(dreams_array);
 
         adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, dreams_array);
 
-        //dream_list = (ListView) findViewById(R.id.list);
         dream_list = getListView();
         dream_list.setAdapter(adapter);
 
     }
-
+    
 
     protected void loadDreams() {
         String[] temp_loaded_dreams;
         SharedPreferences settings = getApplicationContext().getSharedPreferences("stored_dreams", 0);
         String loaded_dreams = settings.getString("dreams", "");
-        temp_loaded_dreams = loaded_dreams.split("~");
+        temp_loaded_dreams = loaded_dreams.split("~>_");
         dreams = new ArrayList<String>(Arrays.asList(temp_loaded_dreams));
 
         if (temp_loaded_dreams[0].isEmpty()) // remove initial empty dream
@@ -75,7 +76,7 @@ public class JournalListActivity extends ListActivity {
         SharedPreferences.Editor editor = settings.edit();
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < dreams.size(); i++) {
-            sb.append(dreams.get(i)).append("~");
+            sb.append(dreams.get(i)).append("~>_");
         }
         editor.putString("dreams", sb.toString());
         editor.apply();
@@ -87,16 +88,43 @@ public class JournalListActivity extends ListActivity {
         Toast.makeText(this, item + " selected", Toast.LENGTH_LONG).show();
     }
 */
+
     @Override
-    protected void onListItemClick(ListView dream_list, View v, int position, long id) {
+    public boolean onKeyDown(int keyCode, KeyEvent event)
+    {
+        if ((keyCode == KeyEvent.KEYCODE_BACK))
+        {
+            saveDreams();
+            startActivity(new Intent(JournalListActivity.this, JournalActivity.class));
+            return false;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    /*
+     *  When you click on an item (dream), highlight it, and set its setSelected value to true
+     */
+    @Override
+    protected void onListItemClick(ListView dream_list, View v, int position, long id) { // Position is 0 indexed
         //dreams.remove(position);
 
+        Button deleteButton = (Button) findViewById(R.id.button11);
+        deleteButton.setAlpha(1.0f);
+        deleteButton.setClickable(true);
+        String item = String.valueOf(position);
+        Toast.makeText(this, item + " selected", Toast.LENGTH_LONG).show();
 
+        for (int i = 0; i < dream_list.getCount(); i++) {
+            dream_list.getChildAt(i).setBackgroundColor(Color.WHITE);
+            dream_list.getChildAt(i).setSelected(false);
+        }
+        //this.dream_list
+        dream_list.getChildAt(position).setBackgroundColor(Color.LTGRAY);
+        dream_list.getChildAt(position).setSelected(true);
         //saveDreams();
 
 
         //startActivity(getIntent());
-
 
     }
 
@@ -110,5 +138,33 @@ public class JournalListActivity extends ListActivity {
                 startActivity(new Intent(JournalListActivity.this, MenuActivity.class));
             }
         });
+    }
+
+    public void setUpDeleteButton(){
+        Button deleteButton = (Button) findViewById(R.id.button11);
+
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deleteDream();
+            }
+        });
+    }
+
+    public void deleteDream(){
+
+        String item = String.valueOf(dream_list.getCount());
+        Toast.makeText(this, item + " number of items", Toast.LENGTH_LONG).show();
+
+        for (int i = 0; i < dream_list.getCount(); i++)
+        {
+            if (dream_list.getChildAt(i).isSelected()) {
+                dream_list.getChildAt(i).setBackgroundColor(Color.RED);
+                dreams.remove(i);
+                saveDreams();
+                finish();
+                startActivity(getIntent());
+            }
+        }
     }
 }
