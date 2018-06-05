e65b0b4511a5363f1936ca195cdd7e9264944af7
Sean Patrick Malloy <spmalloy@ucdavis.edu>
2018-06-05 15:20:49 -0700

    Changed how checks work
    If variable is null or empty, it will input a -1

--- Javascript/forum.js
+++ Javascript/forum.js
@@ -1,67 +1,63 @@
-var firestore = firebase.firestore();
-var records = document.getElementById("recordings");
+var firestore = firebase.firestore(); //initialize firestore
+//Get Global values
+var records = document.getElementById("recordings"); 
 records.style.display = "flex";
 var sortButton = document.getElementById("sorting");
 var tileNum = 0;
 var sort = document.getElementById("sort").value;
 var order = document.getElementById("order").value;
-    for(var i = 0; i < tileNum; i++){
-        var tileID = "tile:" + i;
-        var oldTiles = document.getElementById(tileID);
-        if(oldTiles != null){
-            records.removeChild(oldTiles);
-        }
-    }
-    tileNum = 0;
-    var sort = document.getElementById("sort").value;
-    var order = document.getElementById("order").value;
-    firestore.collection('USERS').doc('null').collection('records').orderBy(sort, order).limit(100).get().then(function(querySnapshot) {
-        querySnapshot.forEach(function(doc) {
-            var tile = document.createElement('div');
+
+//Access the Null/forum database from firestore, and display the records/forum
+//Limit show by 100
+firestore.collection('USERS').doc('null').collection('records').orderBy(sort, order).limit(100).get().then(function(querySnapshot) {
+    querySnapshot.forEach(function(doc) { // For each Log
+        //Make a new tile element and populate it with data
+            var tile = document.createElement('div'); 
             tile.className = "tile";
             tile.id = "tile:" + tileNum;
             tileNum++;
-            if(doc.data().sound1 != undefined && doc.data().sound1.trim() != ""){
+        //if the data isnt empty or undefined, add it to the tile
+            if(doc.data().sound1 != undefined && doc.data().sound1.trim() != "-1"){
                 var sound1 = document.createElement("p");
                 sound1.textContent = "Sound 1: " + doc.data().sound1;
                 tile.appendChild(sound1);
             }
-            if(doc.data().sound2 != undefined && doc.data().sound2.trim() != ""){
+            if(doc.data().sound2 != undefined && doc.data().sound2.trim() != "-1"){
               var sound2 = document.createElement("p");
               sound2.textContent = "Sound 2: " + doc.data().sound2;
               tile.appendChild(sound2);
             }
-            if(doc.data().freq1 != undefined && doc.data().freq1.trim() != ""){
+            if(doc.data().freq1 != undefined && doc.data().freq1.trim() != "-1"){
               var freq1 = document.createElement("p");
               freq1.textContent = "freq 1: " + doc.data().freq1;
               tile.appendChild(freq1); 
             }
-            if(doc.data().freq2 != undefined && doc.data().freq2.trim() != ""){
+            if(doc.data().freq2 != undefined && doc.data().freq2.trim() != "-1"){
               var freq2 = document.createElement("p");
               freq2.textContent = "freq 2: " + doc.data().freq2;
               tile.appendChild(freq2);  
             }
-            if(doc.data().amp1 != undefined && doc.data().amp1.trim() != ""){
+            if(doc.data().amp1 != undefined && doc.data().amp1.trim() != "-1"){
               var amp1 = document.createElement("p");
               amp1.textContent = "amplitude 1: " + doc.data().amp1;
               tile.appendChild(amp1);
             }
-            if(doc.data().amp2 != undefined && doc.data().amp2.trim() != ""){
+            if(doc.data().amp2 != undefined && doc.data().amp2.trim() != "-1"){
               var amp2 = document.createElement("p");
               amp2.textContent = "amplitude 2: " + doc.data().amp2;
               tile.appendChild(amp2);
             }
-            if(doc.data().rating != undefined && doc.data().rating.trim() != ""){
+            if(doc.data().rating != undefined && doc.data().rating.trim() != "-1"){
               var rating = document.createElement("p");
               rating.textContent = "rating: " + doc.data().rating;
               tile.appendChild(rating);
             }
-            if(doc.data().exp != undefined && doc.data().exp.trim() != ""){
+            if(doc.data().exp != undefined && doc.data().exp.trim() != "-1"){
               var exp = document.createElement("p");
               exp.textContent = "Experience: " + doc.data().exp;
               tile.appendChild(exp);
             }
-            if(doc.data().tags != undefined && doc.data().tags.trim() != ""){
+            if(doc.data().tags != undefined && doc.data().tags.trim() != "-1"){
               var tags = document.createElement("p");
               tags.textContent = "Tags: " + doc.data().tags;
               tile.appendChild(tags);
@@ -74,7 +70,9 @@ var order = document.getElementById("order").value;
         });
     });
 
+//This is the function for sorting, this gets called when you clock the sort button
 sortButton.addEventListener("click", function(){
+    //This removes all the old tiles
     for(var i = 0; i < tileNum; i++){
         var tileID = "tile:" + i;
         var oldTiles = document.getElementById(tileID);
@@ -83,6 +81,7 @@ sortButton.addEventListener("click", function(){
         }
     }
     tileNum = 0;
+    //This is the same as above, but it is sorting it by the vale we want to sort it with!
     var sort = document.getElementById("sort").value;
     var order = document.getElementById("order").value;
     firestore.collection('USERS').doc('null').collection('records').orderBy(sort, order).limit(100).get().then(function(querySnapshot) {
@@ -91,47 +90,47 @@ sortButton.addEventListener("click", function(){
             tile.className = "tile";
             tile.id = "tile:" + tileNum;
             tileNum++;
-            if(doc.data().sound1.trim() != ""){
+            if(doc.data().sound1 != undefined && doc.data().sound1.trim() != "-1"){
                 var sound1 = document.createElement("p");
                 sound1.textContent = "Sound 1: " + doc.data().sound1;
                 tile.appendChild(sound1);
             }
-            if(doc.data().sound2.trim() != ""){
+            if(doc.data().sound2 != undefined && doc.data().sound2.trim() != "-1"){
               var sound2 = document.createElement("p");
               sound2.textContent = "Sound 2: " + doc.data().sound2;
               tile.appendChild(sound2);
             }
-            if(doc.data().freq1.trim() != ""){
+            if(doc.data().freq1 != undefined && doc.data().freq1.trim() != "-1"){
               var freq1 = document.createElement("p");
               freq1.textContent = "freq 1: " + doc.data().freq1;
               tile.appendChild(freq1); 
             }
-            if(doc.data().freq2.trim() != ""){
+            if(doc.data().freq2 != undefined && doc.data().freq2.trim() != "-1"){
               var freq2 = document.createElement("p");
               freq2.textContent = "freq 2: " + doc.data().freq2;
               tile.appendChild(freq2);  
             }
-            if(doc.data().amp1.trim() != ""){
+            if(doc.data().amp1 != undefined && doc.data().amp1.trim() != "-1"){
               var amp1 = document.createElement("p");
               amp1.textContent = "amplitude 1: " + doc.data().amp1;
               tile.appendChild(amp1);
             }
-            if(doc.data().amp2.trim() != ""){
+            if(doc.data().amp2 != undefined && doc.data().amp2.trim() != "-1"){
               var amp2 = document.createElement("p");
               amp2.textContent = "amplitude 2: " + doc.data().amp2;
               tile.appendChild(amp2);
             }
-            if(doc.data().rating.trim() != ""){
+            if(doc.data().rating != undefined && doc.data().rating.trim() != "-1"){
               var rating = document.createElement("p");
               rating.textContent = "rating: " + doc.data().rating;
               tile.appendChild(rating);
             }
-            if(doc.data().exp.trim() != ""){
+            if(doc.data().exp != undefined && doc.data().exp.trim() != "-1"){
               var exp = document.createElement("p");
               exp.textContent = "Experience: " + doc.data().exp;
               tile.appendChild(exp);
             }
-            if(doc.data().tags.trim() != ""){
+            if(doc.data().tags != undefined && doc.data().tags.trim() != "-1"){
               var tags = document.createElement("p");
               tags.textContent = "Tags: " + doc.data().tags;
               tile.appendChild(tags);
--- Javascript/logDream.js
+++ Javascript/logDream.js
@@ -2,8 +2,11 @@ var firestore = firebase.firestore();
 //
 //Database Functions
 //
+//This checks if you are logged in
 firebase.auth().onAuthStateChanged(function(user) {
+    //If your logged in
   if (user) {
+      //get global variables
       document.getElementById("inputs").style.display = "block";
       document.getElementById("login").style.display = "none";
       var currentUser = firebase.auth().currentUser.email;
@@ -11,6 +14,7 @@ firebase.auth().onAuthStateChanged(function(user) {
       var docRef = firestore.collection('USERS').doc(currentUser).collection('records');
       var saveButton = document.getElementById("submit");
       var docSettings = firestore.collection('USERS').doc(currentUser).collection('settings').doc('settings');
+      
       docSettings.get().then(function(doc) {
         if (doc.exists) {
             settings = doc.data().forum;
@@ -26,16 +30,43 @@ firebase.auth().onAuthStateChanged(function(user) {
 
     saveButton.addEventListener("click", function(){
     var sound1 = document.querySelector("#sound1").value;
+    if(sound1.trim() == ""){
+        sound1 = '-1';
+    }
     var freq1 = document.querySelector("#freq1").value;
+    if(isNaN(freq1) || freq1.trim() == ""){
+        freq1 = '-1';
+    }
     var amp1 = document.querySelector("#amp1").value;
+    if(amp1.trim() == ""){
+        amp1 = '-1';
+    }
     var sound2 = document.querySelector("#sound2").value;
+    if(sound2.trim() == ""){
+        sound2 = '-1';
+    }
     var freq2 = document.querySelector("#freq2").value;
+    if(isNaN(freq2) || freq2.trim() == ""){
+        freq2 = '-1';
+    }
     var amp2 = document.querySelector("#amp2").value;
+    if(amp2.trim() == ""){
+        amp2 = '-1';
+    }
     var tags = document.querySelector("#tags").value;
+    if(tags.trim() == ""){
+        tags = '-1';
+    }
     var dateTime = new Date();
     var date = "Day: " + dateTime.getFullYear() + ":" + dateTime.getMonth() + ":" + dateTime.getDate() + " Time: " + dateTime.getHours() + ":" + dateTime.getMinutes() + ":" + dateTime.getSeconds();
     var exp = document.querySelector("#exp").value;
+    if(exp.trim() == ""){
+        exp = "-1";
+    }
     var rating = document.getElementById("rating").value;
+    if(rating.trim() == ""){
+        rating = '-1';
+    }
     
     
     console.log("I am trying to save input to Firestore");
@@ -81,6 +112,7 @@ firebase.auth().onAuthStateChanged(function(user) {
             });
         }
     })
+      //IF your not logged in, it brings you to the homepage!
   } else {
       location.replace("./index.html");
       document.getElementById("inputs").style.display = "none";
