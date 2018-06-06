f953ed91cf762d4f97ed4c1de2065b4fc2b6d336
jeffnwarner <jnwarner@ucdavis.edu>
2018-06-06 12:01:20 -0700

    added new variable to LucidApp so journal isn't constantly being pushed to github

--- app/src/main/java/com/example/g/luciddreamgenerator/LucidApp.java
+++ app/src/main/java/com/example/g/luciddreamgenerator/LucidApp.java
@@ -4,12 +4,21 @@ import android.app.Application;
 
 public class LucidApp extends Application{
     private boolean loggedIn  = false;
+    private boolean recentlyLogged = false;
     private boolean isSynced = false;
     public boolean getLogged() {
         return loggedIn;
     }
+    public boolean getRecentlyLogged() {
+        return recentlyLogged;
+    }
+
 
     public void setLogged(boolean loggedIn) {
         this.loggedIn = loggedIn;
+        setRecentlyLogged(loggedIn);
+    }
+    public void setRecentlyLogged(boolean loggedIn) {
+        this.recentlyLogged = loggedIn;
     }
 }
