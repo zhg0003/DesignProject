d5fcfd974d051d87c4fce308af9e501acbaaf8b9
jeffnwarner <jnwarner@ucdavis.edu>
2018-06-06 12:02:11 -0700

    Now menu not only pulls dream logs, but also pushes dream logs on the first instance of being logged in

--- app/src/main/java/com/example/g/luciddreamgenerator/MenuActivity.java
+++ app/src/main/java/com/example/g/luciddreamgenerator/MenuActivity.java
@@ -18,6 +18,7 @@ import android.support.v7.app.ActionBar;
 import android.widget.Button;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -26,13 +27,18 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 public class MenuActivity extends AppCompatActivity {
 
     boolean isLoggedin = false;
+    boolean recentlyLogged = false;
     String username;
+    String currentDate;
     List<String> dreams = new LinkedList<String>();
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -49,6 +55,7 @@ public class MenuActivity extends AppCompatActivity {
 
         try {
             isLoggedin = ((LucidApp) getApplication()).getLogged();
+            recentlyLogged = ((LucidApp) getApplication()).getRecentlyLogged();
             username = user.getEmail();
         }
         catch (NullPointerException e) {}
@@ -60,8 +67,16 @@ public class MenuActivity extends AppCompatActivity {
         setUpSyncButton();
         setUpLoginText();
 
+        if (recentlyLogged)
+            pushCurrentDreams();
+
         if (isLoggedin)
             syncWithDatabase();
+
+        try {
+            ((LucidApp) getApplication()).setRecentlyLogged(false);
+        }
+        catch (NullPointerException e) {}
     }
 
     protected void syncWithDatabase(){
@@ -86,8 +101,8 @@ public class MenuActivity extends AppCompatActivity {
                                 dream = dream.concat(" - ");
                                 dream = dream.concat(exp + '\n');
                                 dream = dream.concat("Sound(s) Played During Dream:" + '\n');
-                                dream = dream.concat(" -Tone at " + freq1 + "hz" + '\n');
-                                dream = dream.concat(" -Tone at " + freq2 + "hz" + '\n');
+                                dream = dream.concat(sound1 + " at " + freq1 + "hz" + '\n');
+                                dream = dream.concat(sound2 + " at " + freq2 + "hz" + '\n');
 
                                 dreams.add(dream);
 
@@ -123,6 +138,85 @@ public class MenuActivity extends AppCompatActivity {
                         editor.putString("database_dreams", sb.toString());
                         editor.apply();
 
+                    }
+                });
+    }
+
+    protected void pushCurrentDreams() {
+        String[] temp_loaded_dreams;
+        List<String> local_dreams = new LinkedList<String>();
+        SharedPreferences settings = getApplicationContext().getSharedPreferences("stored_dreams", 0);
+        String loaded_dreams = settings.getString("dreams", "");
+        temp_loaded_dreams = loaded_dreams.split("~>_");
+        local_dreams = new ArrayList<String>(Arrays.asList(temp_loaded_dreams));
+
+        if (temp_loaded_dreams[0].isEmpty()) // remove initial empty dream
+            local_dreams.clear();
+        else {
+            try {
+                for (int c = 0; c < local_dreams.size(); c++) {
+                    Map<String, Object> data = new HashMap<>();
+                    String[] split_dream_entry = local_dreams.get(c).split("\n- ");
+                    for (int i = 0; i < split_dream_entry.length; i++) {
+                        if (i == 0) {
+                            data.put("date", split_dream_entry[i].substring(1));
+                            currentDate = split_dream_entry[i].substring(1);
+                        } else if (i == 1) {
+                            data.put("exp", split_dream_entry[i]);
+                        } else if (i == 2) {
+                            String[] split_tones = split_dream_entry[i].split("\n  *");
+                            String[] split_first_tone = split_tones[1].split(" ");
+                            data.put("sound1", split_first_tone[0]);
+                            data.put("freq1", split_first_tone[2].substring(0, split_first_tone[2].length() - 2));
+                            if (split_tones.length > 2) {
+                                String[] split_second_tone = split_tones[2].split(" ");
+                                data.put("sound2", split_second_tone[0]);
+                                data.put("freq2", split_second_tone[2].substring(0, split_second_tone[2].length() - 2));
+                            }
+                            else {
+                                data.put("sound2", " ");
+                                data.put("freq2", " ");
+                            }
+                        } else if (i == 3 && split_dream_entry[i].substring(0, 13).compareTo("Custom Fields") == 0) {
+                            String[] split_custom = split_dream_entry[i].split("\n  *");
+                            data.put("tags", split_custom[1]);
+
+                        } else if (i == 4 || split_dream_entry[i].substring(0, 13).compareTo("Dream Quality") == 0) {
+                            String[] split_rating = split_dream_entry[i].split(" ");
+                            data.put("rating", split_rating[2]);
+                        }
+                    }
+                    insertDb(data);
+                }
+            }
+            catch(NullPointerException e){
+                Snackbar snackbar;
+                snackbar = Snackbar.make(getWindow().getDecorView().getRootView(),
+                        "Error pushing journal",
+                        10000);
+                View snackbarView = snackbar.getView();
+                TextView textView = (TextView) snackbarView.findViewById(android.support.design.R.id.snackbar_text);
+                textView.setMaxLines(5);
+                snackbar.show();
+            }
+        }
+    }
+    protected void insertDb(Map<String, Object> userData) {
+        // Add a new document with a generated ID
+        db.collection("USERS/" + username + "/records/").document(currentDate)
+                .set(userData)
+
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Snackbar snackbar;
+                        snackbar = Snackbar.make(getWindow().getDecorView().getRootView(),
+                                "Error adding document",
+                                10000);
+                        View snackbarView = snackbar.getView();
+                        TextView textView = (TextView) snackbarView.findViewById(android.support.design.R.id.snackbar_text);
+                        textView.setMaxLines(5);
+                        snackbar.show();
                     }
                 });
     }
